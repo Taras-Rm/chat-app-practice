91795159efcea094134afa5bb72b5768d606d353
Taras Romaniuk <tarasrom25@gmail.com>
2024-05-16 09:41:47 +0300

    added basic connection with socket io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("ws://localhost:3001", { autoConnect: false });
+
 function App() {
+  useEffect(() => {
+    socket.connect();
+  }, []);
   return <div>App</div>;
 }
 
---
+++ server/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected ", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected ", socket.id);
+  });
+});
+
+export default server;
